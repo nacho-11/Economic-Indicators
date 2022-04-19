582398090ad584439a8662102df2a653473afd85
Juan Salas-Morales <jisalas1@uc.cl>
2022-04-18 21:20:21 -0400

    :sparkles: Fetch indicator values

--- src/ducks/indicators.js
+++ src/ducks/indicators.js
@@ -1,9 +1,11 @@
 // Initial State
 const initialState = {
   list: { data: [], loading: false },
+  indicatorValues: { data: {}, loading: false },
 }
 // Actions
 const SET_INDICATORS = 'INDICATORS/SET_INDICATORS'
+const SET_INDICATOR_VALUES = 'INDICATORS/SET_INDICATOR_VALUES'
 
 // Reducer
 export default function reducer(state = initialState, action) {
@@ -11,6 +13,8 @@ export default function reducer(state = initialState, action) {
   switch (type) {
     case SET_INDICATORS:
       return { ...state, list: payload }
+    case SET_INDICATOR_VALUES:
+      return { ...state, indicatorValues: payload }
     default:
       return state
   }
@@ -19,6 +23,11 @@ export default function reducer(state = initialState, action) {
 // Action Creators
 export const setIndicators = payload => ({
   type: SET_INDICATORS,
+  payload,
+})
+
+export const setIndicatorValues = payload => ({
+  type: SET_INDICATOR_VALUES,
   payload,
 })
 
@@ -38,3 +47,14 @@ export const fetchIndicators = () => async (dispatch, getState, services) => {
   })
   dispatch(setIndicators({ data, loading: false }))
 }
+
+export const fetchIndicatorValues =
+  payload => async (dispatch, getState, services) => {
+    dispatch(setIndicatorValues({ data: [], loading: true }))
+    const { get, mindicadorApi } = services
+    const { indicator } = payload
+
+    const result = await get(`${mindicadorApi}/${indicator}`)
+
+    dispatch(setIndicatorValues({ data: result, loading: false }))
+  }
