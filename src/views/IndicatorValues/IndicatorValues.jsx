4cc7d6db4cb8492afd5775432a32ab0924078439
Juan Salas-Morales <jisalas1@uc.cl>
2022-04-18 21:20:49 -0400

    :sparkles: Show indicator values

--- src/views/IndicatorValues/IndicatorValues.jsx
+++ src/views/IndicatorValues/IndicatorValues.jsx
@@ -1,12 +1,41 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
-import { Text, View } from 'react-native'
+import { Skeleton } from '@rneui/themed'
+import { ScrollView, View } from 'react-native'
+import { useDispatch, useSelector } from 'react-redux'
+
+import { IndicatorValueCard } from '../../components'
+import { fetchIndicatorValues } from '../../ducks/indicators'
 
 function IndicatorValues(props) {
   const { codigo } = props.route.params
+  const { data, loading } = useSelector(
+    state => state.Indicators.indicatorValues,
+  )
+
+  const { serie } = data
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(fetchIndicatorValues({ indicator: codigo }))
+  }, [dispatch, codigo])
+
+  if (loading) {
+    return (
+      <View>
+        <Skeleton variant="rectangular" width={'100%'} height={50} />
+      </View>
+    )
+  }
+
   return (
     <View>
-      <Text>Hola</Text>
+      <ScrollView>
+        {serie?.map((item, i) => (
+          <IndicatorValueCard fecha={item.fecha} valor={item.valor} key={i} />
+        ))}
+      </ScrollView>
     </View>
   )
 }
