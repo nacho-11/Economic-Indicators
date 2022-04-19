ecd6d2685c678ce8e83e37917f74498aafa40c62
Juan Salas-Morales <jisalas1@uc.cl>
2022-04-18 20:46:16 -0400

    :sparkles: List indicators

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,14 +1,13 @@
 import React, { useEffect } from 'react'
 
-import { View, Text } from 'react-native'
+import { View, ScrollView } from 'react-native'
 import { useDispatch, useSelector } from 'react-redux'
 
+import { IndicatorCard } from '../components'
 import { fetchIndicators } from '../ducks/indicators'
 
 function Home(props) {
   const { loading, data } = useSelector(state => state.Indicators)
-
-  console.log(data)
 
   const dispatch = useDispatch()
 
@@ -18,7 +17,11 @@ function Home(props) {
 
   return (
     <View>
-      <Text>Hello World!</Text>
+      <ScrollView>
+        {Object.values(data).map((indicator, i) => (
+          <IndicatorCard data={indicator} key={i} />
+        ))}
+      </ScrollView>
     </View>
   )
 }
