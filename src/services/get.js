4b8cf54250533bda61369766e5ff1a895437cab6
Juan Salas-Morales <jisalas1@uc.cl>
2022-04-19 20:52:47 -0400

    :recycle: Change fetch per axios

--- src/services/get.js
+++ src/services/get.js
@@ -1,13 +1,16 @@
+import axios from 'axios'
+
 export default async function get(url) {
   try {
-    const response = await fetch(url, {
+    const response = await axios.get(url, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
       },
     })
-    return await response.json()
+    return response.data
   } catch (err) {
     console.error(err)
+    throw err
   }
 }
